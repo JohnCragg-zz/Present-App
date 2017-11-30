90ac32f6c412d9797dfa87d40414be270e3917ac
Mark Holmes <reserve@hotmail.co.uk>
2017-11-30 22:10:45 +0000

    Played around to get comfortable. Changed Person attributes. Created uninteresting Item class. Third time lucky

--- python/src/test/test_parser.py
+++ python/src/test/test_parser.py
@@ -6,14 +6,19 @@ from python.src.main.parser import Parser
 
 class TestParser(TestCase):
     def setUp(self):
-        self.parser = Parser("mark,holmes,23,bellend")
+        self.parser = Parser("1,ledge@sheg.com,jonald,cregg")
 
     def test_can_parse_a_string_into_a_class(self):
         actual = self.parser.create_person()
-        expected = Person("mark", "holmes", 23, "bellend")
+        expected = Person(1, "ledge@sheg.com", "jonald", "cregg")
+        self.assertEqual(actual.personal_id, expected.personal_id)
+        self.assertEqual(actual.email, expected.email)
         self.assertEqual(actual.first_name, expected.first_name)
-        self.assertEqual(actual.second_name, expected.second_name)
-        self.assertEqual(actual.age, expected.age)
-        self.assertEqual(actual.description, expected.description)
+        self.assertEqual(actual.last_name, expected.last_name)
 
 print("herpy derp")
+
+cregg = Parser("1,ledge@sheg.com,jonald,cregg")
+cregg_the_person = cregg.create_person()
+cregg_the_person.say_hello()
+cregg_the_person.print_email_provider()
