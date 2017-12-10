6c682026e3725e5a720e84a769ef1ea24ef1ca82
John Cragg <john.l.cragg@gmail.com>
2017-12-10 16:31:45 +0000

    parameterised and renamed the insert person method to enable reuse

--- python/src/main/db.py
+++ python/src/main/db.py
@@ -1,28 +1,37 @@
 import sqlite3
 from sqlite3 import Error
+from python.src.main.Person import Person
+
+mark = Person(1, 'SparkyMark123@grinder.com', "Mark", "Holmes")
+john = Person(2, "FredSaidIWasBoring@loser.com", "John", "Cragg")
+people_mark_cares_about = [mark, john]
+
+
+def create_person_table(c):
+    c.execute(
+        "CREATE TABLE IF NOT EXISTS Person(personal_id REAL, email TEXT, first_name TEXT, last_name TEXT)"
+    )
+
+
+def insert_person(c, conn, person):
+    c.execute("INSERT INTO Person VALUES(%d, '%s', '%s', '%s')"
+              % (person.personal_id, person.email, person.first_name, person.last_name))
+    conn.commit()
 
 
 def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-
     try:
         conn = sqlite3.connect(db_file)
         c = conn.cursor()
+        create_person_table(c)
 
-        def create_table():
-            c.execute(
-                "CREATE TABLE IF NOT EXISTS Person(personal_id REAL, email TEXT, first_name TEXT, last_name TEXT)"
-            )
+        for person in people_mark_cares_about:
+            insert_person(c, conn, person)
+            print("Inserted person, %s, into the database" % person.first_name)
 
-        def data_entry():
-            c.execute("INSERT INTO Person VALUES(1, 'mark@aol.com', 'Mark', 'Holmes')")
-            c.execute("INSERT INTO Person VALUES(2, 'john@aol.com', 'John', 'Cragg')")
-            conn.commit()
-            c.close()
-
-        create_table()
-        data_entry()
-        print(sqlite3.version)
+        c.close()
+        print("populated table")
+        
     except Error as e:
         print(e)
     finally:
@@ -30,4 +39,4 @@ def create_connection(db_file):
 
 
 if __name__ == '__main__':
-    create_connection("mark.db")
+    create_connection("person.db")
