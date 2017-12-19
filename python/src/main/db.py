5a2367f0982cd685044c9de4d81de3a7e48fd593
John Cragg <john.l.cragg@gmail.com>
2017-12-19 20:15:02 +0000

    refactoring

--- python/src/main/db.py
+++ python/src/main/db.py
@@ -13,128 +13,67 @@ ps2 = Item(3, 'Playstation 2', 60, 4, 'www.game.com')
 bs = Item(3, 'Banana Suit', 55, 1, 'wwww.clerksfresh.org')
 clarkes_xmas_list = [bike, ps2, bs]
 
-# 'create' methods
 
 def create_person_table(c):
     c.execute(
         "CREATE TABLE IF NOT EXISTS Person(personal_id INT, email TEXT, first_name TEXT, last_name TEXT)"
     )
 
+
 def create_item_table(c):
     c.execute(
         "CREATE TABLE IF NOT EXISTS Item(person_id INT, name TEXT, price REAL, priority INT, hyperlink TEXT)"
     )
 
-# 'insert' methods
 
 def insert_person(c, conn, person):
     c.execute("INSERT INTO Person VALUES('%d', '%s', '%s', '%s')"
               % (person.personal_id, person.email, person.first_name, person.last_name))
     conn.commit()
 
+
 def insert_item(c, conn, item):
     c.execute("INSERT INTO Item VALUES('%d', '%s', '%d', '%d', '%s')"
               % (item.person_id, item.name, item.price, item.priority, item.hyperlink))
     conn.commit()
 
-# 'create connection' methods
 
-def create_connection_person(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-        create_person_table(c)
+def populate_table(lst, conn, c, insert_method):
+    for element in lst:
+        insert_method(c, conn, element)
+        print("Inserted element, %s, into the database" % element)
+    c.close()
 
-        for person in people_mark_cares_about:
-           insert_person(c, conn, person)
-           print("Inserted person, %s, into the database" % person.first_name)
 
-        c.close()
-        print("populated table")
+def create_connection():
+    c = conn.cursor()
+    create_person_table(c)
+    create_item_table(c)
 
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
+    populate_table(people_mark_cares_about, conn, c, insert_person)
+    populate_table(clarkes_xmas_list, conn, c, insert_item)
+    return c
 
-def create_connection_item(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-        create_item_table(c)
 
-        for item in clarkes_xmas_list:
-            insert_item(c, conn, item)
-            print("Inserted item, %s, into the database" % item.name)
+def read_person_table(c):
+    c.execute("SELECT * FROM Person")
+    for row in c:
+        print(row)
 
-        c.close()
-        print("populated table")
 
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
+def read_item_table(c):
+    c.execute("SELECT * FROM Item")
+    for row in c:
+        print(row)
 
-def create_connection(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-        d = conn.cursor()
-        create_person_table(c)
-        create_item_table(d)
-
-        for person in people_mark_cares_about:
-           insert_person(c, conn, person)
-           print("Inserted person, %s, into the database" % person.first_name)
-
-        c.close()
-        print("populated table")
-
-        for item in clarkes_xmas_list:
-            insert_item(d, conn, item)
-            print("Inserted item, %s, into the database" % item.name)
-
-        d.close()
-        print("populated table")
-
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
 
 if __name__ == '__main__':
-#    create_connection_person("person.db")
-#    create_connection_item("item.db")
-    create_connection("person_and_item.db")
-
-def read_person_table(db_file):
     try:
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-
-        c.execute("SELECT * FROM Person")
-        for row in c:
-            print(row)
-
+        conn = sqlite3.connect("person_and_item.db")
+        cursor = create_connection()
+        read_person_table(cursor)
+        read_item_table(cursor)
     except Error as e:
         print(e)
     finally:
         conn.close()
-
-def read_item_table(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        c = conn.cursor()
-
-        c.execute("SELECT * FROM Item")
-        for row in c:
-            print(row)
-
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
-
-read_person_table("person_and_item.db")
-read_item_table("person_and_item.db")
-
