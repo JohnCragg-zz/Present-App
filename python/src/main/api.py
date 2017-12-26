da5d1fc0c11482f359bbf18338f5e5e9ecca2f56
John Cragg <john.l.cragg@gmail.com>
2017-12-26 18:32:11 +0000

    connected api

--- python/src/main/api.py
+++ python/src/main/api.py
@@ -1,31 +1,61 @@
-from flask import Flask, jsonify, abort
+import logging
+import sqlite3
 
+from flask import Flask
+from flask import jsonify, abort
+
+from python.src.main.db import DB
 from python.src.main.parser import create_person, create_item
+from python.src.main.Person import Person
 
-app = Flask(__name__)
 
+def main():
+    app = Flask(__name__)
+    logging.basicConfig(filename='present_app.log', level=logging.DEBUG)
+    conn = sqlite3.connect("person_and_item.db")
+    cursor = conn.cursor()
+    db = DB(cursor, conn)
+    db.create_person_table()
+    db.create_item_table()
 
-class Api(object):
-    def __init__(self, db):
-        self.db = db
+    app.run(debug=True)
+    print("app hosted at http://127.0.0.1:5000/")
 
-    @app.route('/present/api/v1/person/<str:email>', methods=['GET'])
-    def get_person(self, email):
-        person_from_db = self.db.get_person(email)
+    @app.route('/present/api/v1/person/<string:person_info>', methods=['PUT'])
+    def put_person(person_info):
+        try:
+            (email, first_name, second_name) = person_info.split(",")
+            db.insert_person(Person(email, first_name, second_name))
+            logging.info("Put person with email '%s'" % email)
+        except ValueError:
+            logging.error("Not enough arguments passed to url" % person_info)
+
+    @app.route('/present/api/v1/person/<string:email>', methods=['GET'])
+    def get_person(email):
+        person_from_db = db.get_person(email)
         if len(person_from_db) > 1:
+            logging.error("Multiple people with email '%s'" % email)
             return KeyError("multiple primary keys for key '%s'" % email)
         elif len(person_from_db) == 0:
+            logging.error("Couldn't find person with email '%s'" % email)
             abort(404)
         else:
+            logging.info("Fetched person with email '%s'" % email)
             return jsonify({'person': create_person(person_from_db[0])})
 
-    @app.route('/present/api/v1/item/<str:email>', methods=['GET'])
-    def get_person(self, email):
-        items_from_db = self.db.get_items(email)
-        items = []
-        if len(items_from_db) == 0:
-            abort(404)
-        else:
-            for i in items_from_db:
-                items += create_item(i)
-            return jsonify({'items': items})
+    # @app.route('/present/api/v1/item/<string:email>', methods=['GET'])
+    # def get_person(email):
+    #     items_from_db = db.get_items(email)
+    #     items = []
+    #     if len(items_from_db) == 0:
+    #         abort(404)
+    #     else:
+    #         for i in items_from_db:
+    #             items += create_item(i)
+    #         return jsonify({'items': items})
+
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
--- python/src/main/db.py
+++ python/src/main/db.py
@@ -1,3 +1,7 @@
+import logging
+from sqlite3 import IntegrityError
+
+
 class DB(object):
     def __init__(self, cursor, conn):
         self.cursor = cursor
@@ -8,16 +12,21 @@ class DB(object):
         self.cursor.execute(
             "CREATE TABLE IF NOT EXISTS Person(email TEXT PRIMARY KEY, first_name TEXT, last_name TEXT)"
         )
+        logging.info("Person table created")
 
     def create_item_table(self):
         self.cursor.execute(
             "CREATE TABLE IF NOT EXISTS Item(person_email TEXT, name TEXT, price REAL, priority INT, hyperlink TEXT)"
         )
+        logging.info("Item table created")
 
     def insert_person(self, person):
-        self.cursor.execute("INSERT INTO Person VALUES('%s', '%s', '%s')"
-                            % (person.email, person.first_name, person.last_name))
-        self.conn.commit()
+        try:
+            self.cursor.execute("INSERT INTO Person VALUES('%s', '%s', '%s')"
+                                % (person.email, person.first_name, person.last_name))
+            self.conn.commit()
+        except IntegrityError:
+            logging.error("Key '%s' was not unique and hasn't been entered into the db" % person.email)
 
     def insert_item(self, item):
         self.cursor.execute("INSERT INTO Item VALUES('%s', '%s', '%d', '%d', '%s')"
@@ -27,7 +36,7 @@ class DB(object):
     def populate_table(self, lst, insert_method):
         for element in lst:
             insert_method(element)
-            print("Inserted element, %s, into the database" % element)
+            logging.info("Inserted element, %s, into the database" % element)
 
     def get_items(self, email):
         items = []
